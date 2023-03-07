4996da76032bdbff58117cd366fd2507d27586d6
Zack Teater <zteater@gmail.com>
2023-03-06 22:46:17 -0500

    Add stats when switching areas

--- sh/item-brief-find.sh
+++ sh/item-brief-find.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+
 python3 py/item_brief_find.py "$*"
--- sh/map-stats.sh
+++ sh/map-stats.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
-#count of areas
-echo $(ls maps | wc -l)
+export map="./maps/"$@".map"
 
-# count of rooms
-echo $(grep -rni "R {" maps/* | wc -l)
+# linked areas
+export linked=$(grep 'R {' "$map" | grep '<fff>' | grep '{}{}{}{}{}' | wc -l)
+echo $linked
 
-# count of paths
-echo $(grep -rni "E {" maps/* | wc -l)
+# count of mapped rooms
+export mapped=$(grep 'R {' "$map" | wc -l)
+echo $mapped
+
+# count of unmapped rooms
+export unmapped=$(grep 'R {' "$map" | grep '<efa>' | wc -l)
+echo $unmapped
+
+# total rooms
+export total=$((mapped + unmapped))
+echo $total
+
+# percent mapped
+export percent=$((mapped * 100 / total * 100 / 100))
+echo $percent
